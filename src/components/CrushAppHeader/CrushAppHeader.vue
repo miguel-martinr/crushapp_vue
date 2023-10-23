148b4459526d219a5703bb5d7ba1e8f05ad0f67e
Miguel Alejandro Martín <miguel.martinr11@gmail.com>
2023-10-24 00:10:44 +0100

    header fix

--- src/components/CrushAppHeader/CrushAppHeader.vue
+++ src/components/CrushAppHeader/CrushAppHeader.vue
@@ -1,17 +1,5 @@
-<template>  
-    <div class="row mt-3">
-      <!-- Vertically aligned -->
-      <div class="col-sm-1">
-        <img src="../../assets/crushapp_logo.png" alt="logo" class="img-fluid"/>
-      </div>
-      <div class="col-sm-2 align-self-center">
-        <h1 class="crushapp-title">CrushApp</h1>
-      </div>
-      <div class="col-sm-7">
-        <NavBar />
-      </div>
-    </div>
-  
+<template>
+  <NavBar />
 </template>
 
 <script src="./CrushAppHeader.view.js"></script>
--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,35 +1,28 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container-fluid">
 
-<nav class="navbar navbar-expand-lg navbar-light ">
-  <div class="container-fluid">
-    <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbar">
-      <ul class="navbar-nav">
-        <li v-for="link in navLinks" :key="link.name" class="nav-item" @click="clickLink(link)">
-          <a :class="'nav-link ' + (link.active ? 'active' : '') " href="#">{{ link.label }}</a>
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li> -->
-      </ul>
+      <a class="navbar-brand" href="#">
+        <img src="../../assets/crushapp_logo.png" alt="logo" class="img-fluid"/>
+        <span>CrushApp</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+        aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse ms-5" id="navbar">
+        <ul class="navbar-nav">
+          <li v-for="link in navLinks" :key="link.name" class="nav-item" @click="clickLink(link)">
+            <a :class="'nav-link ps-2 pe-2 ' + (link.active ? 'active' : '')" href="#">{{ link.label }}</a>
+          </li>
+
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
 
+  </nav>
 </template>
 
-<style scoped src="./NavBar.css"> </style>
+<style scoped src="./NavBar.css"></style>
 <script src="./Navbar.view.js"></script>
 
